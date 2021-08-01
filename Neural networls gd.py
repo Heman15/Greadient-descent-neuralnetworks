3562eb1375877844ec331d2cf1cecd1106b987bd
Heman <64802671+Heman15@users.noreply.github.com>
2021-08-01 23:14:05 +0530

    Update Neural networls gd.py

--- Neural networls gd.py
+++ Neural networls gd.py
@@ -6,25 +6,25 @@ Created on Fri Jul 30 23:24:18 2021
 """
 #Implementation of Gradient Descent
 #Define Sigmoid Function
-import numpy as np
+import numpy as np #created sigmoid function
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 #define derivative of sigmoid function
 def sigmoid_der(x):
     return sigmoid(x) * (1- sigmoid(x))
-input = np.array([0.5 ,0.8])
-y = .8
-weights = np.array([-0.8,0.6])
-learning_rate= 0.5
-function = np.dot(input ,weights)
-print(function)
-output = sigmoid(function)
+input = np.array([0.5 ,0.8]) #input values
+y = .8  #target value
+weights = np.array([-0.8,0.6])  #defining intial weights
+learning_rate= 0.5 #defining learning  rate
+function = np.dot(input ,weights) #get f(Y) = x1w1 +x2w2 +x3w3 +x4w4 + .......
+print(function) 
+output = sigmoid(function) #geting output
 print(output)    
-error = y-output
+error = y-output #calculating error between target value and outputvalue
 print(error)
-output_grad = sigmoid_der(output)
+output_grad = sigmoid_der(output)    #applying activation function
 print(output_grad)
-error_term =error *output_grad
+error_term =error *output_grad  #cal. error term
 print(error_term)
-fnal = learning_rate*error_term*input
+fnal = learning_rate*error_term*input       #final output gd step
 print(fnal)
